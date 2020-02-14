04491c5f372b5947e1710a250b1b96eabbc6aeaa
tavo <gutavorjs@gmail.com>
2020-02-13 20:23:07 -0500

    primer commit del proyecto

---
+++ taller1/ejercicio1.js
@@ -0,0 +1,10 @@
+
+console.log("hola mundo")
+console.log(21)
+let num1
+num1 = 2
+console.log(num1)
+let num2 = 5
+let resultado = num1 + num2
+console.log ("el resultado es:" +resultado)
+alert("el resultado es :"+ resultado)
